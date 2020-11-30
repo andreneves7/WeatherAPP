57d8f4d126e4f1e99b14c03f276f8709d64d4cb1
jatsm <jatsmneves@gmail.com>
2020-11-30 20:06:03 +0000

    Final com localizaçao do user e customizada

--- app/src/main/java/com/example/weather/ListActivity.kt
+++ app/src/main/java/com/example/weather/ListActivity.kt
@@ -1,14 +1,35 @@
 package com.example.weather
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
+import android.util.Log
+import android.widget.Button
 import android.widget.ListView
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 
 class ListActivity : AppCompatActivity() {
 
-    lateinit var listView : ListView
+    lateinit var listView: ListView
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var locationUpdateState = false
+
+    lateinit var localizacao: Button
+
+    var latitude = 0.0000000
+    var longitude = 0.0000000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,94 +37,299 @@ class ListActivity : AppCompatActivity() {
         listView = findViewById(R.id.listView)
         var list = mutableListOf<Model>()
 
-        list.add(Model("London",      R.drawable.london  ))
-        list.add(Model("Lisboa",       R.drawable.portugal   ))
-        list.add(Model("Madrid",       R.drawable.madrid   ))
-        list.add(Model("Paris",       R.drawable.paris   ))
-        list.add(Model("Berlim",       R.drawable.berlim   ))
-        list.add(Model("Copenhaga",       R.drawable.copenhaga   ))
-        list.add(Model("Roma",       R.drawable.roma   ))
-        list.add(Model("Dublin",       R.drawable.dublin   ))
-        list.add(Model("Praga",       R.drawable.praga   ))
-        list.add(Model("Viena",       R.drawable.viena   ))
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        listView.adapter = ListAdapter(this,R.layout.row,list)
 
-        listView.setOnItemClickListener{parent, view, position, id ->
 
-            if (position==0){
-                val nome = "London"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
-                }
-                startActivity(intent)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+
+                lastLocation = p0.lastLocation
+
             }
-            if (position==1){
-                val nome = "Lisboa"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+        }
+
+
+        createLocationRequest()
+
+        localizacao = findViewById(R.id.bLocal)
+
+        localizacao.setOnClickListener {
+
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+
+                    if (location != null) {
+                        lastLocation = location
+                        Log.d("Mapa", "$lastLocation}")
+                        Log.d("Mapa", "latitude ${location.latitude}}")
+                        Log.d("Mapa", "longitude ${location.longitude}}")
+                        latitude = location.latitude.toDouble()
+                        longitude = location.longitude.toDouble()
+                        val marca = 1
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.putExtra("marca", marca.toString())
+                        intent.putExtra("latitude", latitude.toString())
+                        intent.putExtra("longitude", longitude.toString())
+
+                        startActivity(intent)
+                    }
                 }
-                startActivity(intent)
-            }
-            if (position==2){
-                val nome = "Madrid"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+
+
+
+
+
+        }
+
+
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                ListActivity.Location_Result_Code
+            )
+
+            return
+        }
+
+
+
+        list.add(Model("London", R.drawable.london))
+        list.add(Model("Lisboa", R.drawable.portugal))
+        list.add(Model("Madrid", R.drawable.madrid))
+        list.add(Model("Paris", R.drawable.paris))
+        list.add(Model("Berlim", R.drawable.berlim))
+        list.add(Model("Copenhaga", R.drawable.copenhaga))
+        list.add(Model("Roma", R.drawable.roma))
+        list.add(Model("Dublin", R.drawable.dublin))
+        list.add(Model("Praga", R.drawable.praga))
+        list.add(Model("Viena", R.drawable.viena))
+
+        listView.adapter = ListAdapter(this, R.layout.row, list)
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+
+            when (position){
+                0 -> {
+                    val nome = "London"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
                 }
-                startActivity(intent)
-            }
-            if (position==3){
-                val nome = "Paris"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+
+                1->{
+                    val nome = "Lisboa"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
                 }
-                startActivity(intent)
-            }
-            if (position==4){
-                val nome = "Berlim"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+
+                2->{
+                    val nome = "Madrid"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
                 }
-                startActivity(intent)
-            }
-            if (position==5){
-                val nome = "Copenhaga"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+
+                3->{
+                    val nome = "Paris"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
                 }
-                startActivity(intent)
-            }
-            if (position==6){
-                val nome = "Roma"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+
+                4->{
+                    val nome = "Berlim"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
                 }
-                startActivity(intent)
-            }
-            if (position==7){
-                val nome = "Dublin"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+
+                5->{
+                    val nome = "Copenhaga"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
                 }
-                startActivity(intent)
-            }
-            if (position==8){
-                val nome = "Praga"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+
+                6->{
+                    val nome = "Roma"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
                 }
-                startActivity(intent)
-            }
-            if (position==9){
-                val nome = "Viena"
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+
+                7->{
+                    val nome = "Dublin"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
                 }
-                startActivity(intent)
+
+                8->{
+                    val nome = "Praga"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
+                }
+
+                9->{
+                    val nome = "Viena"
+                    val intent = Intent(this, MainActivity::class.java).apply {
+                        putExtra(AlarmClock.EXTRA_MESSAGE, nome)
+                    }
+                    startActivity(intent)
+                }
             }
 
         }
 
+    }
+
+
+    companion object {
+        private const val Location_Result_Code = 111
+        private const val REQUEST_CHECK_SETTINGS = 2
+
+
+    }
+
+    private fun startLocationUpdates() {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), Location_Result_Code
+            )
+
+            return
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            Location_Result_Code -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+
+                } else {
+
+                }
+                return
+            }
+
+
+        }
+    }
+
+    private fun createLocationRequest() {
+
+        locationRequest = LocationRequest()
+
+        locationRequest.interval = 10000
+
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            startLocationUpdates()
+        }
+        task.addOnFailureListener { e ->
+
+            if (e is ResolvableApiException) {
+
+                try {
+
+                    e.startResolutionForResult(
+                        this@ListActivity,
+                        REQUEST_CHECK_SETTINGS
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+
+                }
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                locationUpdateState = true
+                startLocationUpdates()
+            }
+        }
+
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if (!locationUpdateState) {
+            startLocationUpdates()
+        }
     }
 
 
--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -5,6 +5,7 @@ import android.icu.number.NumberRangeFormatter.with
 import android.os.AsyncTask
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -20,28 +21,42 @@ class MainActivity : AppCompatActivity() {
 
     var CITY: String = ""
     var IconTempo: String = ""
+    var Lat : String = ""
+    var Long : String = ""
+
     //val API: String ="1b5ce9dd86efb73dab71ebd469cce738"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
 
+        var marca = 0
         val message = intent.getStringExtra(EXTRA_MESSAGE)
         CITY = message.toString()
 
+        val m = intent.getStringExtra("marca")
+        Log.d("Mapa", "m  ${m}")
+        if (m != null) {
+            marca = m.toInt()
+        }
 
+        if (marca == 1 ){
+            val latitude= intent.getStringExtra("latitude")
+            val longitude = intent.getStringExtra("longitude")
+
+            Lat = latitude.toString()
+            Long = longitude.toString()
+
+            Log.d("Mapa", "latitude2 ${latitude}}")
+            Log.d("Mapa", "longitude2 ${longitude}}")
+
+            weatherLatLog().execute()
+        }
 
         weatherTask().execute()
     }
 
     inner class weatherTask() : AsyncTask<String, Void, String>() {
-        override fun onPreExecute() {
-            super.onPreExecute()
-            /* Showing the ProgressBar, Making the main design GONE */
-//            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
-//            findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
-//            findViewById<TextView>(R.id.errorText).visibility = View.GONE
-        }
 
         override fun doInBackground(vararg params: String?): String? {
             var response: String?
@@ -86,7 +101,7 @@ class MainActivity : AppCompatActivity() {
                 val iconWeather = weather.getString("icon")
                 IconTempo = iconWeather
 
-                
+
 
 
                 val iconUrl = "http://openweathermap.org/img/wn/04n@2x.png"
@@ -107,6 +122,9 @@ class MainActivity : AppCompatActivity() {
                 findViewById<TextView>(R.id.wind).text = windSpeed
                 findViewById<TextView>(R.id.pressure).text = pressure
                 findViewById<TextView>(R.id.humidity).text = humidity
+
+
+
                 val imagem : ImageView = findViewById(R.id.icon)
 
                 if (IconTempo == "01d" || IconTempo == "01n" ) {
@@ -144,51 +162,130 @@ class MainActivity : AppCompatActivity() {
                     imagem.setImageResource(R.drawable.mist)
                 }
 
-                /* Views populated, Hiding the loader, Showing the main design */
-//                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-//                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
+
 
             } catch (e: Exception) {
-//                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-//                findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
+
             }
 
         }
     }
 
-//    inner class weatherIcon() : AsyncTask<String, Void, String>() {
-//
-//
-//        override fun doInBackground(vararg params: String?): String? {
-//            var resposta: String?
-//            try {
-//                resposta = URL("http://openweathermap.org/img/wn/$IconTempo@2x.png").toString()
-//            } catch (e: Exception) {
-//                resposta = null
-//            }
-//            return resposta
-//        }
-//
-//
-//        override fun onPostExecute(result: String?) {
-//            super.onPostExecute(result)
-//
-//            try {
-//                /* Extracting JSON returns from the API */
-//                val jsonObj = JSONObject(result)
-//
-//
-//
-//
-//                val imagem = findViewById<ImageView>(R.id.icon)
-//
-//
-//
-//            }catch (e: Exception) {
-//
-//            }
-//        }
-//    }
+    inner class weatherLatLog() : AsyncTask<String, Void, String>() {
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response: String?
+
+            try {
+                response =
+                    URL("https://api.openweathermap.org/data/2.5/weather?lat=$Lat&lon=$Long&appid=1b5ce9dd86efb73dab71ebd469cce738").readText(
+                        Charsets.UTF_8
+                    )
+            } catch (e: Exception) {
+                response = null
+            }
+            return response
+        }
+
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                /* Extracting JSON returns from the API */
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+
+                val updatedAt: Long = jsonObj.getLong("dt")
+                val updatedAtText =
+                    "Updated at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(
+                        Date(updatedAt * 1000)
+                    )
+                val temp = main.getString("temp") + "°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min") + "°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max") + "°C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+
+                val sunrise: Long = sys.getLong("sunrise")
+                val sunset: Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+                val iconWeather = weather.getString("icon")
+                IconTempo = iconWeather
+
+
+
+
+                val iconUrl = "http://openweathermap.org/img/wn/04n@2x.png"
+
+                val address = jsonObj.getString("name") + ", " + sys.getString("country")
+
+                /* Populating extracted data into our views */
+                findViewById<TextView>(R.id.address).text = address
+                findViewById<TextView>(R.id.updated_at).text = updatedAtText
+                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
+                findViewById<TextView>(R.id.temp).text = temp
+                findViewById<TextView>(R.id.temp_min).text = tempMin
+                findViewById<TextView>(R.id.temp_max).text = tempMax
+                findViewById<TextView>(R.id.sunrise).text =
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise * 1000))
+                findViewById<TextView>(R.id.sunset).text =
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset * 1000))
+                findViewById<TextView>(R.id.wind).text = windSpeed
+                findViewById<TextView>(R.id.pressure).text = pressure
+                findViewById<TextView>(R.id.humidity).text = humidity
+
+
+
+                val imagem : ImageView = findViewById(R.id.icon)
+
+                if (IconTempo == "01d" || IconTempo == "01n" ) {
+                    imagem.setImageResource(R.drawable.clear_sky)
+                }
+
+                if (IconTempo == "02d" || IconTempo == "02n" ) {
+                    imagem.setImageResource(R.drawable.few_clouds)
+                }
+                if (IconTempo == "03d" || IconTempo == "03n" ) {
+                    imagem.setImageResource(R.drawable.scattered_clouds)
+                }
+
+                if (IconTempo == "04d" || IconTempo == "04n" ) {
+                    imagem.setImageResource(R.drawable.broken_clouds)
+                }
+
+                if (IconTempo == "09d" || IconTempo == "09n" ) {
+                    imagem.setImageResource(R.drawable.shower_rain)
+                }
+
+                if (IconTempo == "10d" || IconTempo == "10n" ) {
+                    imagem.setImageResource(R.drawable.rain)
+                }
+
+                if (IconTempo == "11d" || IconTempo == "11n" ) {
+                    imagem.setImageResource(R.drawable.thunderstorm)
+                }
+
+                if (IconTempo == "13d" || IconTempo == "13n" ) {
+                    imagem.setImageResource(R.drawable.snow)
+                }
+
+                if (IconTempo == "50d" || IconTempo == "50n" ) {
+                    imagem.setImageResource(R.drawable.mist)
+                }
+
+
+
+            } catch (e: Exception) {
+
+            }
+
+        }
+    }
+
 
 
 }
